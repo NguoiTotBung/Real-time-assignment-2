259d60f6977729362681929083fccd22b65be91f
NguoiTotBung <46132343+NguoiTotBung@users.noreply.github.com>
2019-10-08 15:22:05 +0700

    part3/46

--- part3/part_3.adb
+++ part3/part_3.adb
@@ -206,7 +206,11 @@ package body part_3 is
             end if;
             newline;
 
-            driving_command.change_driving_command(PRIO_DIST, speed, 100, direction);
+            if (speed > PWM_Value'Last) then
+                speed := PWM_Value'Last;
+            end if;
+
+            driving_command.change_driving_command(PRIO_DIST, speed, 500, direction);
 
             Next_time := Next_time + Delay_interval;
             delay until Next_time;
