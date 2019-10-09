707e0fcbe6b5c45ed87b3dd7e2542c1fcd7f2640
NguoiTotBung <46132343+NguoiTotBung@users.noreply.github.com>
2019-10-09 19:12:44 +0700

    part4.22

--- part4/part_4.adb
+++ part4/part_4.adb
@@ -241,6 +241,7 @@ package body part_4 is
             elsif (state = cali_white) then
                 white := Light_value(light_sen);
             elsif (state = ready and not printed) then
+                Clear_Screen_Noupdate;
                 put_noupdate("calibration value: ");
                 newline_noupdate;
                 put_noupdate("- black: ");
@@ -275,37 +276,37 @@ package body part_4 is
 
     ----------------------------------------------------------------------------
     -------- display command description every time a new command is issued ----
-    task body DisplayTask is
-        Next_time      : Time := clock;
-        Delay_interval : Time_span := Milliseconds(200);
-
-        speed          : integer := 0;
-        turn_ratio     : float := 0.0;
-
-        old_speed          : integer := 0;
-        old_turn_ratio     : float := 0.0;
-
-        command_count      : integer := 0;
-    begin
-        loop
-            driving_command.read_current_command(speed, turn_ratio);
-
-            if (old_speed /= speed or old_turn_ratio /= turn_ratio) then
-                Clear_Screen_Noupdate;
-                put_noupdate("command: ");
-                put_noupdate(command_count);
-                Newline_Noupdate;
-                Put_Noupdate("- speed: ");
-                Put_Noupdate(speed);
-                Newline_Noupdate;
-                Put_Noupdate("- turn ratio: ");
-                Put_Noupdate(integer(turn_ratio * 100.0));
-                newline;
-            end if;
-
-            Next_time := Next_time + Delay_interval;
-            delay until Next_time;
-        end loop;
-    end DisplayTask;
+--      task body DisplayTask is
+--          Next_time      : Time := clock;
+--          Delay_interval : Time_span := Milliseconds(200);
+--
+--          speed          : integer := 0;
+--          turn_ratio     : float := 0.0;
+--
+--          old_speed          : integer := 0;
+--          old_turn_ratio     : float := 0.0;
+--
+--          command_count      : integer := 0;
+--      begin
+--          loop
+--              driving_command.read_current_command(speed, turn_ratio);
+--
+--              if (old_speed /= speed or old_turn_ratio /= turn_ratio) then
+--                  Clear_Screen_Noupdate;
+--                  put_noupdate("command: ");
+--                  put_noupdate(command_count);
+--                  Newline_Noupdate;
+--                  Put_Noupdate("- speed: ");
+--                  Put_Noupdate(speed);
+--                  Newline_Noupdate;
+--                  Put_Noupdate("- turn ratio: ");
+--                  Put_Noupdate(integer(turn_ratio * 100.0));
+--                  newline;
+--              end if;
+--
+--              Next_time := Next_time + Delay_interval;
+--              delay until Next_time;
+--          end loop;
+--      end DisplayTask;
 
 end part_4;
--- part4/part_4.ads
+++ part4/part_4.ads
@@ -45,10 +45,10 @@ package part_4 is
         pragma Storage_Size(2048);
     end LightSensorTask;
 
-    task DisplayTask is
-        pragma Priority(System.Priority'Last - 5);
-        pragma Storage_Size(1048);
-    end DisplayTask;
+--      task DisplayTask is
+--          pragma Priority(System.Priority'Last - 5);
+--          pragma Storage_Size(1048);
+--      end DisplayTask;
 
     procedure background;
 end part_4;
